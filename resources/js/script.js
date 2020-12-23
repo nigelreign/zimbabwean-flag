57e78907a4c8d67addbe8ce0bdc426058d8b7905
nigelreign <zulunigelb@gmail.com>
2020-12-23 13:50:42 +0200

    finally made the flag to wave. Yayy

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,6 +3,7 @@ window.onload = function(){
 	drawFlag( flag, 320, 10, 130 );
 	flag.style.marginLeft = -(flag.width/2)+'px';
 	flag.style.marginTop  = -(flag.height/2)+'px';
+	var timer = waveFlag( flag, 20, 10, 150, 200, -0.1 );
 };
 
 
@@ -71,4 +72,40 @@ function drawFlag( canvas, width, padX, padY ){
           ctx.fillStyle = "#fff";
 
 }
+function waveFlag( canvas, wavelength, amplitude, period, shading, squeeze ){
+	if (!squeeze)    squeeze    = 0;
+	if (!shading)    shading    = 100;
+	if (!period)     period     = 200;
+	if (!amplitude)  amplitude  = 10;
+	if (!wavelength) wavelength = canvas.width/10;
 
+	var fps = 30;
+	var ctx = canvas.getContext('2d');
+	var   w = canvas.width, h = canvas.height;
+	var  od = ctx.getImageData(0,0,w,h).data;
+	// var ct = 0, st=new Date;
+	return setInterval(function(){
+		var id = ctx.getImageData(0,0,w,h);
+		var  d = id.data;
+		var now = (new Date)/period;
+		for (var y=0;y<h;++y){
+			var lastO=0,shade=0;
+			var sq = (y-h/2)*squeeze;
+			for (var x=0;x<w;++x){
+				var px  = (y*w + x)*4;
+				var pct = x/w;
+				var o   = Math.sin(x/wavelength-now)*amplitude*pct;
+				var y2  = y + (o+sq*pct)<<0;
+				var opx = (y2*w + x)*4;
+				shade = (o-lastO)*shading;
+				d[px  ] = od[opx  ]+shade;
+				d[px+1] = od[opx+1]+shade;
+				d[px+2] = od[opx+2]+shade;
+				d[px+3] = od[opx+3];
+				lastO = o;
+			}
+		}
+		ctx.putImageData(id,0,0);		
+		// if ((++ct)%100 == 0) console.log( 1000 * ct / (new Date - st));
+	},1000/fps);
+}
